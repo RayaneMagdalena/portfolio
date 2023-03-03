2c462b0ce776afcc0048f2b663c7f288f72927e3
rayane <rayanemagdalena@gmail.com>
2023-03-03 12:07:28 -0300

    animation in javascript

--- scripts.js
+++ scripts.js
@@ -12,3 +12,29 @@ menuToggle.addEventListener("click", () => {
     menuSection.classList.toggle("on", show)
     show = !show;
 })
+
+// ANIMAÇÃO 
+
+const target = document.querySelectorAll('[data-anime]')
+const animatioClass = 'animate';
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 3);
+    target.forEach(function (element) {
+        if ((windowTop) > element.offsetTop) {
+            element.classList.add(animatioClass);
+        } else {
+            element.classList.remove(animatioClass);
+        }
+    })
+}
+
+animeScroll();
+
+if (target.length) {
+    window.addEventListener('scroll', function () {
+        animeScroll();
+    });
+}
+
+
